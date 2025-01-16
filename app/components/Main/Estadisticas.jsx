d30c688f7edef7f33c0252527104e337637b8bbb
jose.gomez <javierrumo2@gmail.com>
2025-01-16 20:55:05 +0100

    ajustar padding

--- app/components/Main/Estadisticas.jsx
+++ app/components/Main/Estadisticas.jsx
@@ -9,7 +9,7 @@ export default function Estadisticas({ peso, repeticiones }) {
             <h3 className="text-xl font-bold w-full text-left mb-4 text-yellow-500">Estadísticas por repeticiones</h3>
             {Array.from({ length: 16 }, (_, i) => (
                 <article key={i} className="w-1/2 sm:w-1/2 md:w-1/4 mt-2 flex justify-center">
-                    <div className="text-center p-4 rounded-lg">
+                    <div className="text-center p-2 rounded-lg">
                         <h4 className="text-lg font-bold text-yellow-500">{i + 1}RM</h4>
                         <p className="text-2xl font-bold text-yellow-300">{Math.round(rm * (1 - 0.025 * i))} kg</p>
                     </div>
